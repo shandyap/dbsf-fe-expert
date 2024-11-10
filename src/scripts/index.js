a4cd54031c6aa5433ee48a5bb57d707f02ce83cc
Shandy Ariady <shandylatoz851@gmail.com>
2024-11-10 22:36:49 +0700

    edit comments

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -28,31 +28,3 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#maincontent').focus();
   });
 });
-
-// // Dynamically populate restaurant list
-// const restoList = document.getElementById('restoList');
-
-// dataRestaurant.forEach((data) => {
-//   const article = document.createElement('article');
-//   article.classList.add('resto-item');
-
-//   const restoThumbnail = `
-//     <div class="resto-thumbnail">
-//       <img src="${data.pictureId}" alt="${data.name}">
-//       <p class="resto-city">${data.city}</p>
-//     </div>
-//   `;
-
-//   const restoContent = `
-//     <div class="resto-item-content">
-//       <p class="resto-item-rating">⭐ ${data.rating}</p>
-//       <h1 >
-//       <a href="#" class="resto-item-title"> ${data.name}</a>
-//       </h1>
-//       <p class="resto-item-description">${data.description}</p>
-//     </div>
-//   `;
-
-//   article.innerHTML = restoThumbnail + restoContent;
-//   restoList.appendChild(article);
-// });
